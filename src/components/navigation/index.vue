27ca7da8350131cfa37ac9e8faffb9a2f684ddfa
吴闯 <wuchuang456@126.com>
2022-05-10 16:02:29 +0800

    tabbar图标切换

--- src/components/navigation/index.vue
+++ src/components/navigation/index.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="navigation">
-    <div class="navigation_1" @click="go('/')">
-      <img src="http://account.channel.bdhuoke.com/img/shouye_2@2x.png" class="navigation_1_1" />
-      <div class="navigation_1_2">首页</div>
-    </div>
-    <div class="navigation_1" @click="go('/vip')">
-      <img src="http://account.channel.bdhuoke.com/img/huiyuan-2@2x.png" class="navigation_1_1" />
-      <div class="navigation_1_2">会员</div>
-    </div>
-    <div class="navigation_1" @click="go('/user')">
-      <img src="http://account.channel.bdhuoke.com/img/wode-2@2x.png" class="navigation_1_1" />
-      <div class="navigation_1_2">我的</div>
+    <div v-for="item in tabbar" :key="item.path" class="navigation_1" @click="go(item.path)">
+      <img :src="item.path === $route.fullPath ? item.activeImg : item.img" class="navigation_1_1" />
+      <div class="navigation_1_2" :class="{active: item.path === $route.fullPath}">{{ item.title }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tabbar: [
+        {
+          title: '首页',
+          img: 'http://account.channel.bdhuoke.com/img/shouye_1@2x.png',
+          activeImg: 'http://account.channel.bdhuoke.com/img/shouye_2@2x.png',
+          path: '/'
+        },
+        {
+          title: '会员',
+          img: 'http://account.channel.bdhuoke.com/img/huiyuan-1@2x.png',
+          activeImg: 'http://account.channel.bdhuoke.com/img/huiyuan-2@2x.png',
+          path: '/vip'
+        },
+        {
+          title: '我的',
+          img: 'http://account.channel.bdhuoke.com/img/wode-1@2x.png',
+          activeImg: 'http://account.channel.bdhuoke.com/img/wode-2@2x.png',
+          path: '/user'
+        }
+      ]
+    }
+  },
   methods: {
     // ? 路由跳转
     go(path) {
